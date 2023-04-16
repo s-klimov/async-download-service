0476b8e4fb4bc6339aad5f16921d7e86f791720b
Sergei Klimov <s.klimov@bk.ru>
2023-04-16 21:44:28 +0700

    Обновлен скрипт sender.py

--- sender.py
+++ sender.py
@@ -3,6 +3,7 @@ import logging
 import sys
 from urllib.parse import urljoin
 
+import aiofiles
 import aiohttp
 
 logging.basicConfig(
@@ -19,27 +20,32 @@ CHUNK_SIZE = 2 ** 16
 FILE_NAME = 'test_photos/7kna/2.jpg'
 
 
-@aiohttp.streamer
-def file_sender(writer, file_name):
+async def file_sender(file_name):
     logger.debug(f'{CHUNK_SIZE=}')
 
-    with open(file_name, 'rb') as f:
-        chunk = f.read(CHUNK_SIZE)
+    async with aiofiles.open(file_name, 'rb') as f:
+        chunk = await f.read(CHUNK_SIZE)
 
         while chunk:
-            logger.debug(f'send a chunk of the file {file_name}')
-            yield from writer.write(chunk)
-            chunk = f.read(CHUNK_SIZE)
+            yield chunk
+            chunk = await f.read(CHUNK_SIZE)
 
 
 async def main():
     url = urljoin(SAVE_FILE_HOST, SAVE_FILE_URL)
-    async with aiohttp.ClientSession() as client:
-        async with client.post(url, data=file_sender(file_name=FILE_NAME)) as resp:
+    headers = {
+        'CONTENT-DISPOSITION': f'attachment;filename={FILE_NAME}',
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, data=file_sender(file_name=FILE_NAME)) as resp:
             logger.info(await resp.text())
 
 
 if __name__ == '__main__':
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
